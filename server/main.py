9bc50c9fa6167e67a56b9f2cc1f5ea15f58b5714
georgesohran <soxrgeorge@gmail.com>
2024-07-25 21:58:30 +0500

    starting work on actual api endpoints

--- server/main.py
+++ server/main.py
@@ -1,19 +1,23 @@
-from fastapi import FastAPI, WebSocket
+from fastapi import FastAPI, WebSocket, WebSocketDisconnect
+from game import *
 
 app = FastAPI()
+game_manager = GameManeger()
 
 
 
-@app.get('/')
-def get1():
-    return {'title': 'hello world'}
+@app.post('/game')
+def create_game(data):
+  game_params = data['game_params']
+  id = game_manager.create_game(game_params)
+  return {'detail': 'success', 'game':game_manager.active_games[id]}
 
 
-app.websocket('/ws')
-async def ws(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        data = await websocket.receive_json()
-        await websocket.send({
-
-        })
+@app.websocket('game/{room_id}/{client_id}')
+async def ws(websocket: WebSocket, room_id:str, client_id:str):
+  await websocket.accept()
+  while True:
+    data = await websocket.receive_json()
+    await websocket.send({
+      
+      })
